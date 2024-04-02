556e97cc23f473c02939d2392ed6520ba41d933f
Javid Ahmed <javidahmed@icloud.com>
2024-04-02 01:04:21 +0100

    Refactor Member

--- src/member.py
+++ src/member.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import List
+
 import numpy as np
 
 
@@ -8,94 +12,93 @@ class Member:
     possible genes. The members can mix their chromosomes via crossover and
     there is a random chance for the genes to mutate during this process,
     controlled by the mutation rate.
+
+    Different simulations will have chromosomes of different types depending on
+    the goal. Chromosome creation and crossover will need to be adjusted to suit
+    the given task. The fitness function will also need to be changed.
     """
 
-    def __init__(self, length, genes, mutation_rate=10):
+    def __init__(self, length: int, gene_pool: List[str], mutation_rate: int) -> None:
         """
         Initialise a member for the population.
 
-        Inputs:
-          length: int, length of chromosome
-          genes: list, all the possible genes
-          mutation_rate: int, probability for a gene to randomly mutate
+        Parameters:
+          length (int): Length of chromosome
+          gene_pool (List[str]): List of possible genes
+          mutation_rate (int): Probability for a gene to randomly mutate
         """
-        self.chromosome = ""
-        self.new_chromosome = ""
-        self.length = length
-        self.genes = genes
+        self._length = length
+        self._gene_pool = gene_pool
+        self._mutation_rate = mutation_rate
+        self._chromosome = ""
+        self._new_chromosome = ""
 
-        self.mutation_rate = mutation_rate
-        self.fitness = 0
-
-        self.generate_chromosome()
-
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Return the member's chromosome.
         """
         return self.chromosome
 
-    def random_char(self):
+    @property
+    def chromosome(self) -> str:
+        """
+        Generate random chromosome if not already created and return.
+        """
+        if not self._chromosome:
+            self._chromosome = "".join([self.random_char for _ in range(self._length)])
+        return self._chromosome
+
+    @property
+    def random_char(self) -> str:
         """
         Return a random gene from the possible genes.
         """
-        return self.genes[np.random.randint(0, len(self.genes))]
+        _choice: str = np.random.choice(self._gene_pool)
+        return _choice
 
-    def generate_chromosome(self):
+    @property
+    def fitness(self) -> int:
         """
-        Generate a random sequence of genes for the chromosome.
+        Return member fitness.
         """
-        self.chromosome = ""
+        return self._score**2
 
-        for _ in range(self.length):
-            self.chromosome += self.random_char()
-
-    def calculate_fitness(self, phrase):
+    def calculate_score(self, phrase: str) -> None:
         """
-        Calculate the member's fitness based on the provided phrase.
+        Calculate the member's score based on the provided phrase.
 
-        Inputs:
-          phrase: str, used to compare chromosome to phrase and calculate fitness
+        Parameters:
+          phrase (str): Used to compare chromosome to phrase and calculate fitness
         """
-        self.fitness = 0
-        score = 0
+        self._score = sum([self.chromosome[i] == phrase[i] for i in range(self._length)])
 
-        for i in range(self.length):
-            if self.chromosome[i] == phrase[i]:
-                score += 1
-
-        self.fitness = score**2
-        return self.fitness
-
-    def crossover(self, parentA, parentB):
+    def crossover(self, parent_a: Member, parent_b: Member) -> None:
         """
         Crossover the chromosomes of two parents to create a new chromosome.
 
-        Inputs:
-          parentA: population_member, used to construct new chromosome
-          parentB: population_member, used to construct new chromosome
+        Parameters:
+          parent_a (Member): Used to construct new chromosome
+          parent_b (Member): Used to construct new chromosome
         """
-        new_chromosome = ""
+        self._new_chromosome = ""
 
-        for i in range(self.length):
+        for i in range(self._length):
             prob = np.random.randint(0, 100)
 
             # Half of the genes will come from parentA
-            if prob < (100 - self.mutation_rate) / 2:
-                new_char = parentA.chromosome[i]
+            if prob < (100 - self._mutation_rate) / 2:
+                new_char = parent_a.chromosome[i]
             # Half of the genes will come from parentB
-            elif prob < (100 - self.mutation_rate):
-                new_char = parentB.chromosome[i]
+            elif prob < (100 - self._mutation_rate):
+                new_char = parent_b.chromosome[i]
             # Chance for a random genes to be selected
             else:
-                new_char = self.random_char()
+                new_char = self.random_char
 
-            new_chromosome += new_char
+            self._new_chromosome += new_char
 
-        self.new_chromosome = new_chromosome
-
-    def apply_new_chromosome(self):
+    def apply_new_chromosome(self) -> None:
         """
         Overwrite the member's chromosome with the new chromosome from crossover.
         """
-        self.chromosome = self.new_chromosome
+        self._chromosome = self._new_chromosome
