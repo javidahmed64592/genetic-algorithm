9a04deefac2f88642b46d67e87b34ab34840cd3f
Javid Ahmed <javidahmed@icloud.com>
2024-04-03 00:15:03 +0100

    Add config file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from src.ga import GeneticAlgorithm
+from src.helpers import load_config
+
+CONFIG_FILE = "config/config.json"
+
 
 if __name__ == "__main__":
-    genes = list(
-        "0123456789 abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.,!?"
-    )  # Genes to be used by the members
-
-    phrase = "I am a genetic algorithm!"  # Phrase to be guessed by the members
-    mutation_rate = 2  # Percentage of genes in chromosomes to mutate
-    population_size = 200  # Number of members in the population
+    ga_config = load_config(CONFIG_FILE)
+    population_size = ga_config["population_size"]  # Number of members in the population
+    mutation_rate = ga_config["mutation_rate"]  # Percentage of genes in chromosomes to mutate
+    phrase = ga_config["phrase"]  # Phrase to be guessed by the members
+    genes = list(ga_config["genes"])  # Genes to be used by the members
 
     mypop = GeneticAlgorithm(population_size, mutation_rate, phrase, genes)
     print(mypop)
