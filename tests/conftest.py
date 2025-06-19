2dfbe1c0a73e90ef0ff358a9f78d6d95b5de7b57
Javid Ahmed <javidahmed@icloud.com>
2025-06-19 23:51:26 +0100

    Refactor docstrings for clarity and consistency across GeneticAlgorithm, Member, and Population classes; enhance test descriptions for better understanding.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+"""Pytest fixtures for neural network unit tests."""
+
 import pytest
 
 from genetic_algorithm.member import Member
@@ -6,6 +8,7 @@ from genetic_algorithm.population import Population
 
 @pytest.fixture
 def mock_member_low_fitness() -> Member:
+    """Fixture to create a mock Member with low fitness."""
     _member = Member()
     _member._chromosome = "123"
     return _member
@@ -13,6 +16,7 @@ def mock_member_low_fitness() -> Member:
 
 @pytest.fixture
 def mock_member_med_fitness() -> Member:
+    """Fixture to create a mock Member with medium fitness."""
     _member = Member()
     _member._chromosome = "1234"
     return _member
@@ -20,6 +24,7 @@ def mock_member_med_fitness() -> Member:
 
 @pytest.fixture
 def mock_member_high_fitness() -> Member:
+    """Fixture to create a mock Member with high fitness."""
     _member = Member()
     _member._chromosome = "12345"
     return _member
@@ -29,6 +34,7 @@ def mock_member_high_fitness() -> Member:
 def mock_population(
     mock_member_low_fitness: Member, mock_member_med_fitness: Member, mock_member_high_fitness: Member
 ) -> Population:
+    """Fixture to create a mock Population with three Members of varying fitness."""
     pop = Population([mock_member_low_fitness, mock_member_med_fitness, mock_member_high_fitness])
     pop.evaluate()
     return pop
--- tests/test_population.py
+++ tests/test_population.py
@@ -1,3 +1,5 @@
+"""Unit tests for the genetic_algorithm/population.py module."""
+
 import numpy as np
 
 from genetic_algorithm.member import Member
@@ -5,14 +7,18 @@ from genetic_algorithm.population import Population
 
 
 class TestPopulation:
+    """Unit tests for the Population class in the genetic_algorithm module."""
+
     def test_given_three_members_when_checking_size_then_check_population_has_right_size(
         self, mock_population: Population
     ) -> None:
+        """Test that the population size is equal to the number of members."""
         assert mock_population.size == len(mock_population._members)
 
     def test_given_three_members_when_getting_best_member_then_check_correct_member_returned(
         self, mock_population: Population, mock_member_high_fitness: Member
     ) -> None:
+        """Test that the best member is the one with the highest fitness."""
         assert mock_population.best_member == mock_member_high_fitness
         assert mock_population.best_member.fitness == mock_population.best_fitness
         assert mock_population.best_member._chromosome == mock_population.best_chromosome
@@ -20,11 +26,13 @@ class TestPopulation:
     def test_given_three_members_when_getting_random_member_then_check_member_in_population(
         self, mock_population: Population
     ) -> None:
+        """Test that a random member is selected from the population."""
         assert mock_population.random_member in mock_population._members
 
     def test_given_parent_when_selecting_another_parent_then_check_different_parent_returned(
         self, mock_population: Population, mock_member_med_fitness: Member
     ) -> None:
+        """Test that a new parent is selected that is different from the given parent."""
         new_parent = mock_population.select_parent(mock_member_med_fitness)
         assert isinstance(new_parent, Member)
         assert new_parent != mock_member_med_fitness
@@ -32,6 +40,7 @@ class TestPopulation:
     def test_given_population_when_evaluating_then_check_population_fitness_calculated(
         self, mock_population: Population
     ) -> None:
+        """Test that the population fitness is calculated correctly."""
         expected_fitness = np.array([member.fitness for member in mock_population._members])
         assert np.array_equal(mock_population._population_fitness, expected_fitness)
         assert mock_population.best_fitness == max(mock_population._population_fitness)
